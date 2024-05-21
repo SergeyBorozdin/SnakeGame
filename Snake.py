4d9fbb788f4fc03e3ff0997d9d09df7283739c16
SergeyBorozdin <94010348+SergeyBorozdin@users.noreply.github.com>
2024-05-21 09:44:57 +0300

    Update Snake.py
    added constans

--- Snake.py
+++ Snake.py
@@ -1,6 +1,12 @@
 import pygame
 
-size = [400,600]
+size = [400, 600]
+FRAME_COLOR = (0, 255, 204)  # цвета в РГБ
+WHITE_COLOR = (255, 255, 255)
+BLUE_COLOR = (204, 255, 255)
+SIZE_BLOCK = 20
+COUNT_BLOCKS = 20
+MARGIN = 1
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Змейка")
@@ -9,3 +15,17 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+    screen.fill(FRAME_COLOR)
+    for row in range(COUNT_BLOCKS):
+        for column in range(COUNT_BLOCKS):
+            if (column + row) % 2 == 0:
+                color = BLUE_COLOR
+            else:
+                color = WHITE_COLOR
+            pygame.draw.rect(screen, color, [10 + column * SIZE_BLOCK + MARGIN * (column + 1),
+                                             20 + row * SIZE_BLOCK + MARGIN * (row + 1),
+                                             SIZE_BLOCK,
+                                             SIZE_BLOCK])
+
+    pygame.display.flip()
